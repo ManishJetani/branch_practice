17307d45adaaf55f9ebf63b31322202ddcf4f73f
ManishJetani <manishjetani14@gmail.com>
2021-08-17 21:42:24 +0100

    ok

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -1,25 +1,23 @@
 package org.example;
 
-public class Hello
-{   int nums [] = {2,7,11,15};
+public class Hello {
+    int nums[] = {2, 7, 11, 15};
     int target = 15;
-    public int[] twoSum()
-        {
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] == target - nums[i]) {
-                        return new int[] { i, j };
-                    }
+
+    public int[] twoSum() {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
                 }
             }
-            throw new IllegalArgumentException("No two sum solution");
         }
-
-    public static void main(String[] args)
-    {
-        Hello hello = new Hello();
-        hello.twoSum();
-        System.out.println("hello and welcome");
+        throw new IllegalArgumentException("No two sum solution");
     }
 
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.twoSum();
+        System.out.println("hello");
+    }
 }
